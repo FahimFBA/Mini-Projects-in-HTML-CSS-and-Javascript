b3f6c0c955de33c823c83aa7079c7b6655e17e75
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2021-04-24 14:59:11 +0600

    Project 9 has been finished

---
+++ Project 09 - Sound Mini-Boards/script.js
@@ -0,0 +1,20 @@
+const sounds = ['reef', 'game', 'rapid', 'floating', 'sea', 'celestial', 'rush']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText = sound
+    btn.addEventListener('click', () => {
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+    document.getElementById('buttons').
+        appendChild(btn)
+})
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+        song.pause()
+        song.currentTime = 0;
+    })
+}
