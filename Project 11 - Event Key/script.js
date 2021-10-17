cfc71634d134200d44c9389aaddc04ee3d6df7dc
FahimFBA <fahimbinamin@gmail.com>
2021-10-18 02:21:52 +0600

    project almost done

---
+++ Project 11 - Event Key/script.js
@@ -0,0 +1,20 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (event) => {
+    insert.innerHTML = `
+    <div class="key">
+    ${event.key === ' ' ? 'Space' : event.Key}
+    <small>event.Key</small>
+</div>
+
+<div class="key">
+   ${event.keyCode}
+    <small>event.keyCode</small>
+</div>
+
+<div class="key">
+    ${event.code}
+    <small>event.Code</small>
+</div>
+    `
+})
